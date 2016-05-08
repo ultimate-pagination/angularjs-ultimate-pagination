1d7645c0342b87e6127676266e62d7abc6a235ea
Dmytro Yarmak <dmytroy@backbase.com>
2016-05-08 23:32:36 +0300

    Basic implementation by using ng-include and provider

---
+++ example/main.js
@@ -0,0 +1,46 @@
+(function() {
+  'use static';
+
+  angular
+    .module('ngUltimatePaginationExample', ['ngUltimatePagination'])
+    .config(config)
+    .run(run)
+    .controller('MainController', MainController);
+
+  config.$inject = ['ultimatePaginationTemplatesProvider'];
+  function config(ultimatePaginationTemplatesProvider) {
+    ultimatePaginationTemplatesProvider.setItemTypeToTemplateUrlMap({
+      PAGE: 'page.html',
+      ELLIPSIS: 'ellipsis.html',
+      FIRST_PAGE_LINK: 'first-page-link.html',
+      PREVIOS_PAGE_LINK: 'previos-page-link.html',
+      NEXT_PAGE_LINK: 'next-page-link.html',
+      LAST_PAGE_LINK: 'last-page-link.html'
+    });
+  }
+
+  run.$inject = ['$templateCache'];
+  function run($templateCache) {
+    $templateCache.put('page.html', '<button ng-style="$ctrl.isActive ? {fontWeight: \'bold\'}: null" ng-click="$ctrl.onClick()">{{::$ctrl.value}}</button>');
+    $templateCache.put('ellipsis.html', '<button ng-click="$ctrl.onClick()">...</button>');
+    $templateCache.put('first-page-link.html', '<button ng-click="$ctrl.onClick()">First</button>');
+    $templateCache.put('previos-page-link.html', '<button ng-click="$ctrl.onClick()">Prev</button>');
+    $templateCache.put('next-page-link.html', '<button ng-click="$ctrl.onClick()">Next</button>');
+    $templateCache.put('last-page-link.html', '<button ng-click="$ctrl.onClick()">Last</button>');
+  }
+
+  MainController.$inject = [];
+  function MainController() {
+    var vm = this;
+
+    vm.currentPage = 1;
+    vm.totalPages = 10;
+    vm.onPageChange = onPageChange;
+
+    //////////
+
+    function onPageChange(newPage) {
+      vm.currentPage = newPage;
+    }
+  }
+}());
--- src/angular-ultimate-pagination.js
+++ src/angular-ultimate-pagination.js
@@ -1,3 +1,109 @@
 import angular from 'angular';
+import {getPaginationModel, ITEM_TYPES} from 'ultimate-pagination';
 
-console.log('angular', angular);
+angular.module('ngUltimatePagination', []);
+
+angular.module('ngUltimatePagination')
+  .provider('ultimatePaginationTemplates', function() {
+    var itemTypeToTemplateUrlMap = null;
+
+    return {
+      setItemTypeToTemplateUrlMap: setItemTypeToTemplateUrlMap,
+      $get: $get
+    };
+
+    //////////
+
+    function setItemTypeToTemplateUrlMap(_itemTypeToTemplateUrlMap_) {
+      itemTypeToTemplateUrlMap = _itemTypeToTemplateUrlMap_
+    }
+
+    $get.$inject = ['$log']
+    function $get($log) {
+      return {
+        getTemplateUrlByItemType: function(itemType) {
+          var templateUrl;
+          if (itemTypeToTemplateUrlMap) {
+            templateUrl = itemTypeToTemplateUrlMap[itemType];
+            if (templateUrl) {
+              return templateUrl;
+            } else {
+              $log.error(`"itemTypeToTemplateUrlMap" does not have a value for item type: ${itemType}`);
+            }
+          } else {
+            $log.error('"itemTypeToTemplateUrlMap" is not registered in "ultimatePaginationTemplatesProvider"');
+          }
+        }
+      };
+    }
+  });
+
+
+angular.module('ngUltimatePagination')
+  .component('ultimatePagination', {
+    template: `
+      <ultimate-pagination-wrapper>
+        <ultimate-pagination-item
+          ng-repeat="item in $ctrl.paginationModel track by item.key"
+          type="::item.type"
+          value="::item.value"
+          is-active="item.isActive"
+          on-click="$ctrl.onItemClick(item)"
+        ></ultimate-pagination-item>
+      </ultimate-pagination-wrapper>
+    `,
+    controller: UltimatePaginationController,
+    replace: true,
+    bindings: {
+      currentPage: '<',
+      totalPages: '<',
+      onChange: '&'
+    }
+  });
+
+UltimatePaginationController.$inject = [];
+function UltimatePaginationController() {
+  var ctrl = this;
+  ctrl.paginationModel = [];
+
+  ctrl.$onInit = function() {
+    calculatePaginationModel();
+  };
+
+  ctrl.onItemClick = function(item) {
+    if (ctrl.currentPage !== item.value) {
+      ctrl.currentPage = item.value;
+      calculatePaginationModel();
+    }
+  }
+
+  function calculatePaginationModel() {
+    ctrl.paginationModel = getPaginationModel({
+      currentPage: ctrl.currentPage,
+      totalPages: ctrl.totalPages
+    });
+  }
+}
+
+angular.module('ngUltimatePagination')
+  .component('ultimatePaginationItem', {
+    template: `
+      <ng-include src="$ctrl.getItemTemplateUrl()"></ng-include>
+    `,
+    controller: UltimatePaginationItemController,
+    bindings: {
+      type: '<',
+      value: '<',
+      isActive: '<',
+      onClick: '&'
+    }
+  });
+
+UltimatePaginationItemController.$inject = ['ultimatePaginationTemplates'];
+function UltimatePaginationItemController(ultimatePaginationTemplates) {
+  var ctrl = this;
+
+  ctrl.getItemTemplateUrl = function() {
+    return ultimatePaginationTemplates.getTemplateUrlByItemType(ctrl.type);
+  }
+}
