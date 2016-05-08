16148abfbf05aa533041fa501eae2f7df4fca80f
Dmytro Yarmak <dmytroy@backbase.com>
2016-05-08 20:00:10 +0300

    Initial build config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/angular-ultimate-pagination.js',
+  output: {
+    path: './dist',
+    library: 'ngUltimatePagination',
+    libraryTarget: 'umd',
+  },
+  externals: ['angular'],
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
